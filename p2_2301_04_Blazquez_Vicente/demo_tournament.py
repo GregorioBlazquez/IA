2113286e4b237bf10cd7c932120ecbba86292a4f
Gregorio Blázquez Martínez <gregorio.blazquez@estudiante.uam.es>
2022-03-09 19:12:50 +0100

    Preparadas 3 heuristicas

--- p2_2301_04_Blazquez_Vicente/demo_tournament.py
+++ p2_2301_04_Blazquez_Vicente/demo_tournament.py
@@ -9,6 +9,8 @@ from __future__ import annotations  # For Python 3.7
 
 import numpy as np
 
+import random
+
 from game import Player, TwoPlayerGameState, TwoPlayerMatch
 from heuristic import simple_evaluation_function, mamadisima_evaluation_function, corners_evaluation_function, movilidad_evaluation_function
 from reversi import (
@@ -19,27 +21,8 @@ from reversi import (
 from tournament import StudentHeuristic, Tournament
 
 
-'''class Heuristic1(StudentHeuristic):
-
-    def get_name(self) -> str:
-        return "dummy"
-
-    def evaluation_function(self, state: TwoPlayerGameState) -> float:
-        # Use an auxiliary function.
-        return self.dummy(123)
-
-    def dummy(self, n: int) -> int:
-        return n + 4
 
 
-class Heuristic2(StudentHeuristic):
-
-    def get_name(self) -> str:
-        return "random"
-
-    def evaluation_function(self, state: TwoPlayerGameState) -> float:
-        return float(np.random.rand())
-'''
 
 class Heuristic1(StudentHeuristic):
 
@@ -50,8 +33,6 @@ class Heuristic1(StudentHeuristic):
         # Use an auxiliary function.
         return corners_evaluation_function(state)
 
-    def dummy(self, n: int) -> int:
-        return n + 4
 
 
 class Heuristic2(StudentHeuristic):
@@ -79,6 +60,28 @@ class Heuristic4(StudentHeuristic):
     def evaluation_function(self, state: TwoPlayerGameState) -> float:
         return mamadisima_evaluation_function(state)
 
+
+class Heuristic5(StudentHeuristic):
+
+    def get_name(self) -> str:
+        return "dummy"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        # Use an auxiliary function.
+        return self.dummy(123)
+
+    def dummy(self, n: int) -> int:
+        return n + 4
+
+
+class Heuristic6(StudentHeuristic):
+
+    def get_name(self) -> str:
+        return "random"
+
+    def evaluation_function(self, state: TwoPlayerGameState) -> float:
+        return float(np.random.rand())
+
 def create_match(player1: Player, player2: Player) -> TwoPlayerMatch:
 
     initial_board = None#np.zeros((dim_board, dim_board))
@@ -90,7 +93,7 @@ def create_match(player1: Player, player2: Player) -> TwoPlayerMatch:
         dim_board=dim_board,
     )"""
 
-    initial_board = (
+    initial_board = [
         ['..B.B...',
         '.WBBW...',
         'WBWBB...',
@@ -98,16 +101,45 @@ def create_match(player1: Player, player2: Player) -> TwoPlayerMatch:
         '.BBWBWB.',
         'WBWBB...',
         '.W.WWW..',
-        '..BWB...']
-    )
+        '..BWB...'],
 
+        ['........',
+        '.WBBW...',
+        'WBWBB...',
+        '..WWWW..',
+        '..BWBWB.',
+        '.BWBB...',
+        '........',
+        '........'],
+
+        ['........',
+        '........',
+        '........',
+        '...WB...',
+        '...BW...',
+        '........',
+        '........',
+        '........'],
+
+        ['........',
+        '.WBWW...',
+        '..WBB...',
+        '..WBBW..',
+        '..BWBWB.',
+        '.BWBW...',
+        '........',
+        '........'],
+
+    ]
+
+    num_board = random.randint(0,len(initial_board)-1)
     if initial_board is None:
         height, width = 8, 8
     else:
         height = len(initial_board)
         width = len(initial_board[0])
         try:
-            initial_board = from_array_to_dictionary_board(initial_board)
+            initial_board = from_array_to_dictionary_board(initial_board[num_board])
         except ValueError:
             raise ValueError('Wrong configuration of the board')
         else:
@@ -130,9 +162,10 @@ def create_match(player1: Player, player2: Player) -> TwoPlayerMatch:
 
 
 tour = Tournament(max_depth=3, init_match=create_match)
-strats = {'opt1': [Heuristic1], 'opt2': [Heuristic2], 'opt3': [Heuristic3], 'opt4': [Heuristic4]}
+strats = {'opt1': [Heuristic1], 'opt2': [Heuristic2], 'opt3': [Heuristic3], 'opt4': [Heuristic4],
+            'opt5': [Heuristic5], 'opt6': [Heuristic6]}
 
-n = 1
+n = 2
 scores, totals, names = tour.run(
     student_strategies=strats,
     increasing_depth=False,
