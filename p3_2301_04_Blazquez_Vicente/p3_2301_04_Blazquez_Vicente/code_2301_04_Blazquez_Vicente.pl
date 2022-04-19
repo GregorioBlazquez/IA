847d12e0fdf05e7d423ec6d898f0ab2733daf369
Gregorio Blázquez Martínez <gregorio.blazquez@estudiante.uam.es>
2022-04-20 00:00:37 +0200

    Ah

--- p3_2301_04_Blazquez_Vicente/p3_2301_04_Blazquez_Vicente/code_2301_04_Blazquez_Vicente.pl
+++ p3_2301_04_Blazquez_Vicente/p3_2301_04_Blazquez_Vicente/code_2301_04_Blazquez_Vicente.pl
@@ -33,9 +33,9 @@ slice([_|Xs],I,K,Ys) :- I > 1,
 *		Resultado: Numero de valor real resultado de la operacion sum_pot_prod. 
 *
 ****************/
-sum_pot_prod(_, _, Potencia, _) :- Potencia<0, print('ERROR 1.1 Potencia.'), !, fail.
-sum_pot_prod([], _, _, _) :- print('ERROR 1.2 Longitud.'), !, fail.
-sum_pot_prod(_, [], _, _) :- print('ERROR 1.2 Longitud.'), !, fail.
+sum_pot_prod(_, _, Potencia, _) :- Potencia<0, write_log('ERROR 1.1 Potencia.'), !, fail.
+sum_pot_prod([], _, _, _) :- write_log('ERROR 1.2 Longitud.'), !, fail.
+sum_pot_prod(_, [], _, _) :- write_log('ERROR 1.2 Longitud.'), !, fail.
 sum_pot_prod([A|[]], [B|[]], Potencia, Resultado) :- Resultado is (A*B)^Potencia, !.
 sum_pot_prod([A|LA],[B|LB], Potencia, Resultado2) :- sum_pot_prod(LA, LB, Potencia, Resultado), Resultado2 is Resultado+(A*B)^Potencia.
 
@@ -52,8 +52,8 @@ sum_pot_prod([A|LA],[B|LB], Potencia, Resultado2) :- sum_pot_prod(LA, LB, Potenc
 ****************/
 penultimo([B,_|[]],B) :- !.
 penultimo([_|L],X) :- penultimo(L,X).
-segundo_penultimo([], _, _) :- print('ERROR 2.1 Longitud.'), !, fail.
-segundo_penultimo([_], _, _) :- print('ERROR 2.1 Longitud.'), !, fail.
+segundo_penultimo([], _, _) :- write_log('ERROR 2.1 Longitud.'), !, fail.
+segundo_penultimo([_], _, _) :- write_log('ERROR 2.1 Longitud.'), !, fail.
 segundo_penultimo([A,B], X, Y) :- X is B, Y is A, !.
 segundo_penultimo([_,A,_], X, Y) :- X is A, Y is A, !.
 segundo_penultimo([_,A|L],X,Y) :- penultimo(L,Y), X is A, !.
@@ -75,15 +75,15 @@ my_length([],0).
 my_length([_|L],N) :- my_length(L,N1), N is N1 + 1.
 
 sliceList([X|_],1,1,[X]) :- !.
-sliceList(_, Menor, Mayor,  _) :- Menor>Mayor, print('ERROR 3.2 Indices.'), !, fail.
-sliceList(L, N, Mayor, _) :- N<Mayor, my_length(L,N), print('ERROR 3.3 Longitud de lista.'), !, fail.
+sliceList(_, Menor, Mayor,  _) :- Menor>Mayor, write_log('ERROR 3.2 Indices.'), !, fail.
+sliceList(L, N, Mayor, _) :- N<Mayor, my_length(L,N), write_log('ERROR 3.3 Longitud de lista.'), !, fail.
 
 sliceList([X|L], 1, Mayor, [X|Sublista]) :- Mayor>1, Mayor1 is Mayor-1,
    sliceList(L, 1, Mayor1, Sublista).
 sliceList([_|L], Menor, Mayor, Sublista) :- Menor>1, 
 	Menor1 is Menor-1, Mayor1 is Mayor-1, sliceList(L, Menor1, Mayor1, Sublista).
 
-contiene([],_) :- print('ERROR 3.1 Elemento.'), !, fail.
+contiene([],_) :- write_log('ERROR 3.1 Elemento.'), !, fail.
 contiene([X|_],Y) :-  Y=X, !.
 contiene([_|L],Y) :- contiene(L, Y).
 
@@ -105,7 +105,7 @@ numElm(Menor, Mayor, Numero_elementos, Incremento) :-
 rejilla(Menor, 1, _, [Menor]) :- !.
 rejilla(Menor, Numero_elementos, Incremento, [Menor|Rejilla]) :-
    Numero_elementos1 is Numero_elementos-1, Menor1 is Menor+Incremento, rejilla(Menor1, Numero_elementos1, Incremento, Rejilla).
-espacio_lineal(Menor, Mayor, _, _) :- Menor>Mayor, print('ERROR 4.1 Indices.'), !, fail.
+espacio_lineal(Menor, Mayor, _, _) :- Menor>Mayor, write_log('ERROR 4.1 Indices.'), !, fail.
 espacio_lineal(Menor, Mayor, Numero_elementos, Rejilla) :- 
    numElm(Menor, Mayor, Numero_elementos, Incremento),
    rejilla(Menor, Numero_elementos, Incremento, Rejilla).
@@ -119,7 +119,7 @@ espacio_lineal(Menor, Mayor, Numero_elementos, Rejilla) :-
 *		Distribucion: Vector de numeros reales de salida. Distribucion normalizada.
 *
 ****************/
-sum([N|_],_) :- N<0, print('ERROR 5.1. Negativos.'), !, fail.
+sum([N|_],_) :- N<0, write_log('ERROR 5.1. Negativos.'), !, fail.
 sum([],0) :- !.
 sum([A|L],Norma) :- sum(L,Sum), Norma is Sum+A.
 
@@ -139,14 +139,14 @@ dividir(Distribucion_sin_normalizar, Norma, Distribucion). /* ¿Hace falta acaba
 *
 ****************/
 distribucion(L) :- sum(L,1.0).
-calculo_kl([],[_|_],_) :- print('ERROR 6.3. Listas de distinto tamaño.'), !, fail.
-calculo_kl([_|_],[],_) :- print('ERROR 6.3. Listas de distinto tamaño.'), !, fail.
+calculo_kl([],[_|_],_) :- write_log('ERROR 6.3. Listas de distinto tamaño.'), !, fail.
+calculo_kl([_|_],[],_) :- write_log('ERROR 6.3. Listas de distinto tamaño.'), !, fail.
 calculo_kl([], [], 0.0).
-calculo_kl([0.0|_], [_|_], _) :- print('ERROR 6.1. Divergencia KL no definida.'), !, fail.
-calculo_kl([_|_], [0.0|_], _) :- print('ERROR 6.1. Divergencia KL no definida.'), !, fail.
+calculo_kl([0.0|_], [_|_], _) :- write_log('ERROR 6.1. Divergencia KL no definida.'), !, fail.
+calculo_kl([_|_], [0.0|_], _) :- write_log('ERROR 6.1. Divergencia KL no definida.'), !, fail.
 calculo_kl([X|LX], [Y|LY], KL) :- calculo_kl(LX, LY, K), KL is K+(X*log(X/Y)).
-divergencia_kl(LX, _, _) :- normalizar(LX,DNX), LX \== DNX, print('ERROR 6.2. Divergencia KL definida solo para distribuciones.'), !, fail.
-divergencia_kl(_, LY, _) :- normalizar(LY,DNY), LY \== DNY, print('ERROR 6.2. Divergencia KL definida solo para distribuciones.'), !, fail.
+divergencia_kl(LX, _, _) :- normalizar(LX,DNX), LX \== DNX, write_log('ERROR 6.2. Divergencia KL definida solo para distribuciones.'), !, fail.
+divergencia_kl(_, LY, _) :- normalizar(LY,DNY), LY \== DNY, write_log('ERROR 6.2. Divergencia KL definida solo para distribuciones.'), !, fail.
 divergencia_kl(LX, LY, KL) :- calculo_kl(LX, LY, KL).
 
 /***************
@@ -160,7 +160,7 @@ divergencia_kl(LX, LY, KL) :- calculo_kl(LX, LY, KL).
 *
 ****************/
 % Calcula el producto de un número fila de la matriz
-producto_fila([X],Y, [S]) :- S is X*Y, S<0, print('ERROR 7.1. Elemento menor que cero.'), !, fail.
+producto_fila([X],Y, [S]) :- S is X*Y, S<0, write_log('ERROR 7.1. Elemento menor que cero.'), !, fail.
 producto_fila([X],Y, [S]) :- S is X*Y, !.
 producto_fila([A|L],Y, [B|R]) :- producto_fila(L,Y,R), B is A*Y.
 
